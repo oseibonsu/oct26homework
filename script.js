f15a0634591f3ecab7cbd2725cb1324253caf963
Daniel Hawn <danieljhawn@gmail.com>
2019-10-29 17:20:50 -0500

    i have no idea what i'm doing

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ var timer = 0;
 var score = 0;
 var startQuiz = document.getElementById("startQuiz");
 var timerDisplay = document.querySelector("#timerDisplay");
-var currentQuestion = questions[0];
+var questionDisplay = document.querySelector("#question");
+// var currentQuestion = questions[0];
 
 
 function startTimer() {
     timer = 200;
-    interval = setInterval(function(){
-      timer--;
-      console.log(timer);
-      timerDisplay.textContent = timer;
+    interval = setInterval(function() {
+        timer--;
+        console.log(timer);
+        timerDisplay.textContent = timer;
     }, 1000);
     document.getElementById("startQuiz").style.visibility = "hidden";
     addQ();
@@ -18,19 +19,37 @@ function startTimer() {
 }
 
 function addQ() {
-    var currentQuestion = questions[currentQuestionIndex];
-    var ques = document.createElement("h1");
-    ques.setAttribute("id", "question");
-    document.getElementById("gameArea").appendChild(ques);
-    var answ = document.createElement("button");
-    answ.setAttribute("id", "choice")
-    document.getElementById("gameArea").appendChild(answ);
-    // var answ = document.createElement("button");
-    // document.getElementById("gameArea").appendChild(answ);
-    // var answ = document.createElement("button");
-    // document.getElementById("gameArea").appendChild(answ);
-    // var answ = document.createElement("button");
-    // document.getElementById("gameArea").appendChild(answ);
+    for (var i = 0; i < questions.length; i++) {
+        var ques = questions[i];
+        var button = document.createElement("button");
+        button.textContent = ques;
+        gameArea.appendChild(button);
+    }
 }
 
-    startQuiz.addEventListener("click", startTimer);
+
+// var currentQuestion = questions;
+// var ques = document.createElement("h1");
+// document.getElementById("gameArea").appendChild(ques);
+// ques.setAttribute("id", "question");
+// currentQuestion.choices.forEach(function(choices) {
+//     choiceNode = document.createElement("button");
+//     choiceNode.setAttribute("class", choice)
+// })
+
+// var currentQuestion = questions[currentQuestionIndex];
+// var ques = document.createElement("h1");
+// ques.setAttribute("id", "question");
+// document.getElementById("gameArea").appendChild(ques);
+// var answ = document.createElement("button");
+// answ.setAttribute("id", "choice")
+// document.getElementById("gameArea").appendChild(answ);
+// var answ = document.createElement("button");
+// document.getElementById("gameArea").appendChild(answ);
+// var answ = document.createElement("button");
+// document.getElementById("gameArea").appendChild(answ);
+// var answ = document.createElement("button");
+// document.getElementById("gameArea").appendChild(answ);
+
+
+startQuiz.addEventListener("click", startTimer);
